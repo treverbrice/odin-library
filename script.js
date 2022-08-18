206fd3338297a538e4c33e5d8ab526df566d725f
treverbrice <treverbrice@gmail.com>
2022-08-18 15:48:21 -0600

    Add initial styles

--- script.js
+++ script.js
@@ -18,5 +18,47 @@ function Book(title, author, pageCount, read) {
 }
 
 function addBookToLibrary(book) {
-    myLibrary.push(book)
+    myLibrary.push(book);
+    updateDisplay();
 }
+
+function updateDisplay() {
+    clearDisplay();
+    const shelf = document.querySelector("#shelf");
+    for (let i = 0; i < myLibrary.length; i++) {
+        const newBook = document.createElement("div");
+        newBook.classList.add("card");
+        const title = document.createElement("div");
+        title.innerText = myLibrary[i].title;
+        const author = document.createElement("div");
+        author.innerText = myLibrary[i].author;
+        const pageCount = document.createElement("div");
+        pageCount.innerText = myLibrary[i].pageCount;
+        const read = document.createElement("div");
+        read.innerText = myLibrary[i].read;
+        newBook.appendChild(title);
+        newBook.appendChild(author);
+        newBook.appendChild(pageCount);
+        newBook.appendChild(read);
+        shelf.appendChild(newBook);
+    }
+}
+
+function clearDisplay() {
+    const shelf = document.querySelector("#shelf");
+    while (shelf.firstChild) {
+      shelf.removeChild(shelf.firstChild);
+    }
+  }
+
+const hobbit = new Book("The Hobbit", "J.R.R. Tolkien", 304, true);
+addBookToLibrary(hobbit);
+
+const fellowship = new Book("The Fellowship of the Ring", "J.R.R. Tolkien", 356, true);
+addBookToLibrary(fellowship);
+
+const twoTowers = new Book("The Two Towers", "J.R.R. Tolkien", 334, false);
+addBookToLibrary(twoTowers);
+
+const rotk = new Book("The Return of the King", "J.R.R. Tolkien", 409, false);
+addBookToLibrary(rotk);
